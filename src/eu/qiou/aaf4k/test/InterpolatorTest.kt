309ab56939a81d818315b188d1784767b3debded
Qiou Yang <6234456@sina.com>
2019-02-24 09:50:31 +0100

    interpolate bugfix round

--- src/eu/qiou/aaf4k/test/InterpolatorTest.kt
+++ src/eu/qiou/aaf4k/test/InterpolatorTest.kt
@@ -11,9 +11,9 @@ class InterpolatorTest {
 
     @Test
     fun parse() {
-        val i = Interpolator(growth = 0.2, period = TimeSpan.forYear(2019), interval = ChronoUnit.MONTHS, numberOfInterval = 3L, interpolationBase = InterpolationBase.END, interpolationType = InterpolationType.ABSOLUTE)
+        val i = Interpolator(growth = 0.2, period = TimeSpan.forYear(2019), interval = ChronoUnit.MONTHS, numberOfInterval = 3L, interpolationBase = InterpolationBase.END, interpolationType = InterpolationType.ABSOLUTE, decimalPosition = 4)
         val p = i.parse()
-        println(p(100.0))
-        println(p(100.0).values.reduce { acc, d -> acc + d })
+        println(p(128.3623))
+        println(p(128.3623).values.reduce { acc, d -> acc + d })
     }
 }
