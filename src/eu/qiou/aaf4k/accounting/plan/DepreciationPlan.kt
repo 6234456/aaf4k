69fc0d2574bae17739ff97e6237fac443206a2c3
Qiou Yang <6234456@sina.com>
2019-03-02 21:25:06 +0100

    AfA-EstG

--- src/eu/qiou/aaf4k/accounting/plan/DepreciationPlan.kt
+++ src/eu/qiou/aaf4k/accounting/plan/DepreciationPlan.kt
@@ -9,14 +9,29 @@ import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
 class DepreciationPlan(
-        val numberOfPeriods: Int, val method: DepreciationMethod = DepreciationMethod.LINEAR,
-        val residualPercentage: Double? = null, val residualAbsoluteValue: Double? = null,
-        val decimalPosition: Int = 2,
-        val depreciationStart: DepreciationStart = DepreciationStart.NEXT_MONTH) {
+        numberOfPeriods: Int = 10,
+        method: DepreciationMethod = DepreciationMethod.LINEAR,
+        residualPercentage: Double? = null,
+        residualAbsoluteValue: Double? = null,
+        private val decimalPosition: Int = 2,
+        private val depreciationStart: DepreciationStart = DepreciationStart.NEXT_MONTH,
+        val customRate: List<Pair<Int, Double>>? = null
+) {
 
-    init {
-        assert(residualAbsoluteValue == null || residualPercentage == null)
+    companion object {
+        // Gewerbe
+        val EstG_7_5_1_1 = DepreciationPlan(customRate = listOf(4 to 0.1, 3 to 0.05, 18 to 0.025))
+        // Wohnimmobilien und sonstige
+        val EstG_7_5_1_3 = DepreciationPlan(customRate = listOf(8 to 0.05, 6 to 0.025, 36 to 0.0125))
     }
+
+    private val method = if (customRate != null) DepreciationMethod.CUSTOMIZED_ANNUAL_RATE else method
+    private val numberOfPeriods = if (customRate != null) customRate.fold(0) { acc, pair -> acc + pair.first }
+    else numberOfPeriods
+    private val residualPercentage = if (customRate != null) 1 - customRate.fold(0.0) { acc, pair -> acc + pair.second * pair.first }
+    else residualPercentage
+
+    private val residualAbsoluteValue = if (customRate != null) null else residualAbsoluteValue
 
     fun generate(assetValue:Double, start: LocalDate): Map<TimeSpan, Double> {
         val start1 = when(depreciationStart){
@@ -26,12 +41,27 @@ class DepreciationPlan(
 
         val residual = residualAbsoluteValue?: (assetValue * residualPercentage!!)
 
+        if (residual < 0) {
+            throw Exception("the residual value can not be negative.")
+        }
+
         return when(method){
-            DepreciationMethod.NUMBER_OF_TOTAL_YEARS ->{
+            DepreciationMethod.NUMBER_OF_TOTAL_YEARS, DepreciationMethod.CUSTOMIZED_ANNUAL_RATE -> {
                 var cnt = -1
-                val t = 1.until(numberOfPeriods).reversed()
-                val total = t.reduce { acc, i -> acc + i }
-                t.map { e -> (e * (assetValue - residual) / total).roundUpTo(decimalPosition) }.let {
+
+                val rates = when (method) {
+                    DepreciationMethod.NUMBER_OF_TOTAL_YEARS -> {
+                        val t = 1.until(numberOfPeriods + 1).reversed()
+                        val total = t.reduce { acc, i -> acc + i }
+                        t.map { e -> e * 1.0 / total }
+                    }
+                    DepreciationMethod.CUSTOMIZED_ANNUAL_RATE -> customRate!!.fold(listOf()) { acc, pair ->
+                        acc + 1.until(pair.first + 1).map { pair.second }
+                    }
+                    else -> throw Exception("Cannot be reached. ERROR!")
+                }
+
+                rates.map { e -> (e * (assetValue - residual)).roundUpTo(decimalPosition) }.let {
                     val delta = (assetValue - residual).roundUpTo(decimalPosition) - it.reduce { acc, d -> acc + d }
                     it.toMutableList().apply {
                         this[lastIndex] += delta
@@ -66,7 +96,8 @@ class DepreciationPlan(
 
 enum class DepreciationMethod {
     LINEAR,
-    NUMBER_OF_TOTAL_YEARS
+    NUMBER_OF_TOTAL_YEARS,
+    CUSTOMIZED_ANNUAL_RATE
 }
 
 enum class DepreciationStart {
--- src/eu/qiou/aaf4k/test/DepreciationPlanTest.kt
+++ src/eu/qiou/aaf4k/test/DepreciationPlanTest.kt
@@ -1,6 +1,5 @@
 package eu.qiou.aaf4k.test
 
-import eu.qiou.aaf4k.accounting.plan.DepreciationMethod
 import eu.qiou.aaf4k.accounting.plan.DepreciationPlan
 import eu.qiou.aaf4k.util.roundUpTo
 import org.junit.Test
@@ -10,7 +9,7 @@ class DepreciationPlanTest {
 
     @Test
     fun generate() {
-        val a = DepreciationPlan( residualPercentage = 0.2, numberOfPeriods = 5, method = DepreciationMethod.NUMBER_OF_TOTAL_YEARS).generate(10000.0, LocalDate.now())
+        val a = DepreciationPlan.EstG_7_5_1_3.generate(1000000.25, LocalDate.now())
         println(a)
         println(a.values.reduce { acc, d -> acc.roundUpTo(2) + d })
     }
