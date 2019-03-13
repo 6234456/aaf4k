0cce74a91a0b8facdd2c10880cc2db4f655920d3
Qiou Yang <6234456@sina.com>
2019-03-13 22:42:49 +0100

    legal doc

--- src/eu/qiou/aaf4k/plugins/CNLegalDocuments.kt
+++ src/eu/qiou/aaf4k/plugins/CNLegalDocuments.kt
@@ -65,15 +65,17 @@ object CNLegalDocuments {
 
         val guid = guid()
 
-        var ywtu: String
+        var ywtu: String = ""
         var f80t: String = ""
         var f80s: String = ""
         var f80t_n: String = ""
 
-        var vjkl5: String
-        var vl5x: String
+        var vjkl5: String = ""
+        var vl5x: String = ""
 
-        var meta: String
+        var meta: String = ""
+
+        var isModel2 = false
 
         val url = urlGen(keywords, guid)
         val b = requestFactory.buildGetRequest(GenericUrl(url))
@@ -82,48 +84,61 @@ object CNLegalDocuments {
 
         b.execute().apply {
             val h = headers["Set-Cookie"].toString()
+            println(h)
 
-            f80s = s1.find(h)!!.groups[1]!!.value
-            f80t = t1.find(h)!!.groups[1]!!.value
-
-            Jsoup.parse(this.parseAsString()).let {
-                meta = it.getElementById("9DhefwqGPrzGxEp9hPaoag").attr("content")
-                ywtu = JSExecutor("script/ywtu.js").invoke("getc", meta).toString()
-            }
-
-            f80t_n = JSExecutor("script/encrypt.js").invoke("getCookies", meta, f80t, ywtu).toString()
-        }
-
-        b.apply {
-            headers.cookie = "FSSBBIl1UgzbN7Nenable=true; $s1_key=$f80s; $t1_key=$f80t_n; wzwsvtime=${java.time.Instant.now().epochSecond}; wzwschallenge=-1; wzwstemplate=NQ==; wzwsconfirm=0e561c10c60c2f0d44410644eb3c2403; _gscbrs_2116842793=1;  _gscs_2116842793=47659453ttzz3o20|pv:14; _gscu_2116842793=47626758817stt18; ccpassport=1ff98c661b8f424096c234ce889da9b0; "
-            headersTraits(headers)
-
-            execute().apply {
-                val h = headers["Set-Cookie"].toString()
-                println(h)
+            if (vjkl5r.containsMatchIn(h)) {
                 vjkl5 = vjkl5r.find(h)!!.groups[1]!!.value
                 vl5x = JSExecutor("script/vl5x.js").invoke("getKey", vjkl5).toString()
 
+                isModel2 = true
+            } else {
+                f80s = s1.find(h)!!.groups[1]!!.value
+                f80t = t1.find(h)!!.groups[1]!!.value
+
                 Jsoup.parse(this.parseAsString()).let {
                     meta = it.getElementById("9DhefwqGPrzGxEp9hPaoag").attr("content")
-                    // ywtu = JSExecutor("script/ywtu.js").invoke("getc", meta).toString()
+                    ywtu = JSExecutor("script/ywtu.js").invoke("getc", meta).toString()
                 }
 
-                f80t_n = JSExecutor("script/encrypt.js").invoke("getCookies", meta, f80t_n, ywtu).toString()
+                f80t_n = JSExecutor("script/encrypt.js").invoke("getCookies", meta, f80t, ywtu).toString()
+            }
+        }
+
+        b.apply {
+            if (!isModel2) {
+                headers.cookie = "FSSBBIl1UgzbN7Nenable=true; $s1_key=$f80s; $t1_key=$f80t_n; wzwsvtime=${java.time.Instant.now().epochSecond}; wzwschallenge=-1; wzwstemplate=NQ==; wzwsconfirm=0e561c10c60c2f0d44410644eb3c2403; _gscbrs_2116842793=1;  _gscs_2116842793=47659453ttzz3o20|pv:14; _gscu_2116842793=47626758817stt18; ccpassport=1ff98c661b8f424096c234ce889da9b0; "
+                headersTraits(headers)
+
+                execute().apply {
+                    val h = headers["Set-Cookie"].toString()
+                    println(h)
+                    vjkl5 = vjkl5r.find(h)!!.groups[1]!!.value
+                    vl5x = JSExecutor("script/vl5x.js").invoke("getKey", vjkl5).toString()
+
+                    Jsoup.parse(this.parseAsString()).let {
+                        meta = it.getElementById("9DhefwqGPrzGxEp9hPaoag").attr("content")
+                        // ywtu = JSExecutor("script/ywtu.js").invoke("getc", meta).toString()
+                    }
+
+                    f80t_n = JSExecutor("script/encrypt.js").invoke("getCookies", meta, f80t_n, ywtu).toString()
+                }
             }
         }
 
         val u = "http://wenshu.court.gov.cn/List/ListContent"
-        val requestData = "Param=${URLEncoder.encode("全文检索", "UTF-8")}:${toKeyWord(keywords)}&Index=$index&Page=10&Order=${URLEncoder.encode("法院层级", "UTF-8")}&Direction=asc&vl5x=$vl5x&guid=$guid"
+        val requestData = "Param=${URLEncoder.encode("全文检索", "UTF-8")}:${toKeyWord(keywords)}&Index=$index&Page=10&Order=${URLEncoder.encode("法院层级", "UTF-8")}&Direction=asc&vl5x=$vl5x&guid=$guid&number=wens"
 
         println(requestData)
         val post = requestFactory.buildPostRequest(GenericUrl(u), ByteArrayContent.fromString(null, requestData))
 
         post.apply {
-            headers.cookie = "FSSBBIl1UgzbN7Nenable=true; $s1_key=$f80s; $t1_key=$f80t_n; vjkl5=$vjkl5; wzwsvtime=${java.time.Instant.now().epochSecond}; wzwschallenge=-1; wzwstemplate=NQ==; wzwsconfirm=0e561c10c60c2f0d44410644eb3c2403; _gscbrs_2116842793=1;  _gscs_2116842793=47659453ttzz3o20|pv:14; _gscu_2116842793=47626758817stt18; ccpassport=1ff98c661b8f424096c234ce889da9b0;"
+            //  headers.cookie = "FSSBBIl1UgzbN7Nenable=true; $s1_key=$f80s; $t1_key=$f80t_n; vjkl5=$vjkl5; wzwsvtime=${java.time.Instant.now().epochSecond}; wzwschallenge=-1; wzwstemplate=NQ==; wzwsconfirm=0e561c10c60c2f0d44410644eb3c2403; _gscbrs_2116842793=1;  _gscs_2116842793=47659453ttzz3o20|pv:14; _gscu_2116842793=47626758817stt18; ccpassport=1ff98c661b8f424096c234ce889da9b0;"
             headersTraits(headers)
 
-            println(execute().parseAsString())
+            execute().apply {
+                println(headers["Set-Cookie"].toString())
+                println(parseAsString())
+            }
         }
     }
 }
