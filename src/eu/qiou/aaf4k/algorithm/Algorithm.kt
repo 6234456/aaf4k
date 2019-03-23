a931ac2eb783bf9ea35cf04ca9595bb96ffb2374
Qiou <yang@qiou.eu>
2019-03-23 05:58:11 +0100

    update Algorithm module

---
+++ src/eu/qiou/aaf4k/algorithm/Algorithm.kt
@@ -0,0 +1,183 @@
+package eu.qiou.aaf4k.algorithm
+
+import eu.qiou.aaf4k.util.mkString
+import kotlin.math.roundToInt
+
+
+object Algorithm {
+
+    val primes = mutableListOf(2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L)
+
+    fun sequentialIsPrime(n:Long):Boolean {
+        if (primes.binarySearch(n) >= 0)
+            return true
+
+        if (preliminaryCheck(n)){
+            val ende = Math.sqrt(n.toDouble()).roundToInt() + 1
+            primes.forEachIndexed { index, l ->
+                if (l > ende) {
+                    primes.add(n)
+                    return true
+                }
+
+                if (index > 2){
+                    if(n.rem(l) == 0L)
+                        return false
+                }
+            }
+        }
+
+        return false
+    }
+
+    private fun preliminaryCheck(n:Long):Boolean{
+        val digit = truncatNumber(n, 1)
+        if (digit.rem(2) == 0L || digit.rem(5) == 0L || threeDividable(n))
+            return false
+
+        return true
+    }
+
+    fun primesBefore(n: Long):List<Long>{
+        if (primes.last() >= n) {
+            var tmp = n
+            while (true){
+                val a = primes.binarySearch(tmp)
+                if(a < 0) tmp-- else return primes.take(a+1)
+            }
+        }
+
+        (primes.last() until(n+1)).forEach { sequentialIsPrime(it) }
+        return primes
+    }
+
+    fun threeDividable(n:Long): Boolean {
+        val len = Algorithm.totalDigit(n) + 1
+        if (len < 3)  return n.rem(3L) == 0L
+
+        return threeDividable((1 until len).fold(0){ acc, i -> acc + digitAt(n, i) }.toLong())
+    }
+
+    fun digitAt(number: Long, position: Int):Int {
+        if( position <= 0 || position > totalDigit(number) )
+            throw Exception("ParameterException: position $position not allowed")
+
+       return (if(position == 1)
+            truncatNumber(number, 1)
+        else
+            (truncatNumber(number, position) - truncatNumber(number, position -1)) / (1 until position-1).fold(10L){acc, _ -> acc * 10L }
+               ).toInt()
+    }
+
+    fun truncatNumber(number: Long, take: Int):Long{
+        val divisor = (1 until take).fold(10L){acc, _ -> acc * 10L }
+        return number.rem(divisor)
+    }
+
+    fun totalDigit(number: Long): Int{
+        var res = 1
+        var divisor = 10L
+        while (true){
+            if (number.rem(divisor) == number)
+                return res
+
+            divisor *= 10L
+            res++
+        }
+    }
+
+
+    fun gcd(a: Long, b: Long):Long{
+        if ( a < 0L || b < 0L)
+            throw Exception("Only nature number supported.")
+
+        if (a == 0L || b == 0L) return 0L
+
+        if (a == b) return a
+
+        return if (a > b){
+            val rem2 = a.rem(b)
+
+             if (rem2 == 0L)
+                 b
+            else
+                 gcd(b, rem2)
+        } else{
+             gcd(b, a)
+        }
+    }
+
+    fun gcd(a:Int, b:Int):Int {
+        return gcd(a.toLong(), b.toLong()).toInt()
+    }
+
+    fun lcm(a: Int, b: Int):Long{
+        return Algorithm.lcm(a.toLong(), b.toLong())
+    }
+
+    fun lcm(a:Long, b:Long):Long {
+        return a * b / gcd(a, b)
+    }
+
+    fun serial(start: Long, generator: (Long)-> Long):List<Long>{
+        val res = mutableListOf<Long>()
+        var a = start
+
+        while (!res.contains(a)){
+            res.add(a)
+            a = generator(a)
+        }
+
+        return res
+    }
+
+    fun gcdSolution(a: Long, b: Long, gcd: Long = gcd(a, b)): Pair<Long, Long>{
+        if (a > b){
+            if (gcd == 0L) return 0L to 0L
+
+            if (b == gcd || a.rem(b) == gcd) return 1L to -1L * (a / b)
+
+            val p = gcdSolution(b, a.rem(b), gcd)
+            return p.second to (p.second * (a / b) * (-1L) + p.first)
+        }
+
+        return gcdSolution(b, a, gcd).let {
+            it.second to it.first
+        }
+    }
+
+    // invoke primeBefore to prepare the prime numbers first
+    fun factorialPrime(n:Long): Map<Long, Int>{
+        if (primes.binarySearch(n) >= 0){
+            return mapOf(n to 1)
+        }
+
+        var tmp = n
+        val res = mutableMapOf<Long, Int>()
+        primes.forEach {
+            if (tmp == 1L)
+                return res
+
+            var cnt = 0
+            while (true){
+                if(tmp.rem(it) != 0L){
+                    if (cnt > 0){
+                        res.put(it, cnt)
+                    }
+                    break
+                }
+                tmp /= it
+                cnt++
+            }
+        }
+
+        return res
+    }
+
+    fun factorialToString(map: Map<Long, Int>):String {
+        return map.map { "${it.key}${if (it.value == 1) "" else "^${it.value}" }" }.mkString("*","","")
+    }
+
+
+
+}
---
+++ src/eu/qiou/aaf4k/test/AlgorithmTest.kt
@@ -0,0 +1,95 @@
+package eu.qiou.aaf4k.test
+
+import eu.qiou.aaf4k.algorithm.Algorithm
+import eu.qiou.aaf4k.algorithm.Algorithm.factorialPrime
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AlgorithmTest {
+
+    @Test
+    fun gcd() {
+        println(Algorithm.gcd(1160718174,316258250))
+        println(Algorithm.gcd(12345,67890))
+        println(Algorithm.gcd(54321,9876))
+        println(Algorithm.gcd(0,9876))
+        println(Algorithm.gcd(51,68))
+        println(Algorithm.gcd(23,18))
+        println(Algorithm.gcd(23,18))
+        println(Algorithm.lcm(301337, 307829))
+        println(Algorithm.gcd(301337, 307829))
+    }
+
+    @Test
+    fun lcm(){
+        val res = 18 * 720
+        var cnt = 1
+        var a = 0
+        while (true){
+            a = 18 * cnt ++
+            if (a > res) break
+            if (res.rem(a) != 0) continue
+
+            val b = res / a
+            if (Algorithm.gcd(a, b) == 18){
+                println("$a , $b")
+            }
+        }
+    }
+
+    @Test
+    fun serial(){
+        val g: (Long) -> Long = { if(it.rem(2) == 0L) it/2 else (it * 3 + 1)}
+
+        (1L until 100L).forEach {
+            println( " ${String.format("%3d", it)}   =   ${Algorithm.serial(it, g)}" )
+        }
+
+    }
+
+    @Test
+    fun gcdS(){
+       println( Algorithm.gcdSolution(155L, 341L))
+       println( Algorithm.gcd(155L, 341L))
+        // -2 + 11k    1 + 5k   1   -2 , 1   x, y
+       println( Algorithm.gcdSolution(385L, 341L))
+       println( Algorithm.gcd(385L, 341L))
+        // 8 + 31j   -9 + 35k   0   0, 0    z,  y
+       println( Algorithm.gcdSolution(155L, 385L))
+       println( Algorithm.gcd(155L, 385L))
+        // 5 + 77i    -2 + 31i   -6   -30, 12,  x,  z
+        // -32  x   1  y   12 z
+        // 155x + 341y + 385z
+        //= 1.
+       println( Algorithm.gcdSolution(37L, 47L))
+    }
+
+    @Test
+    fun test1(){
+        println(Algorithm.totalDigit(12345678))
+        println(Algorithm.truncatNumber(12345, 1))
+        println(Algorithm.digitAt(12345, 1))
+        println(Algorithm.digitAt(123405, 2))
+        println(Algorithm.threeDividable(111111111111))
+        println((1 until 9 + 1).fold(0){ acc, i -> acc + Algorithm.digitAt(111111111, i)})
+    }
+
+    @Test
+    fun test2(){
+        Algorithm.primesBefore(1000030)
+
+
+    }
+
+    @Test
+    fun test3(){
+        Algorithm.primesBefore(1000031)
+        (1000031L downTo(1000000)).forEach {
+            println(Algorithm.factorialToString(factorialPrime(it)))
+        }
+        println(factorialPrime(75460))
+
+
+    }
+}
